6945c94ab795040a1d5914171c2cbd24b59954e5
Markus Fröschle <mfro@mubf.de>
2025-07-18 14:38:22 +0200

    increase waiting time in xdotool to avoid failed runs

--- xjoyce-reload.sh
+++ xjoyce-reload.sh
@@ -4,8 +4,8 @@ WID=$(xdotool search --class xjoyce | head -n 1)
 #echo $WID
 xdotool windowactivate $WID
 xdotool windowfocus --sync $WID
-xdotool key --window $WID --clearmodifiers --delay 200 "F9" "F5" "y" "Return" "2"
+xdotool key --window $WID --clearmodifiers --delay 250 "F9" "F5" "y" "Return" "2"
 sleep 4
-xdotool type --window $WID --clearmodifiers --delay 220 "n:demo"
-xdotool key --window $WID --clearmodifiers --delay 200 "Return"
+xdotool type --window $WID --clearmodifiers --delay 250 "n:demo"
+xdotool key --window $WID --clearmodifiers --delay 250 "Return"
 
