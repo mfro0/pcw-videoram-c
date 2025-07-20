97a57f62f96b2e66cba3182b6ec593e06160b345
Markus Fröschle <mfro@mubf.de>
2025-07-20 15:30:09 +0200

    release the Control key that might have been pressed to start xdotool

--- xjoyce-reload.sh
+++ xjoyce-reload.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
+xdotool keyup Control_L Control_R R		# release Control keys that might have been used from QtCreator
 WID=$(xdotool search --class xjoyce | head -n 1)
 #echo $WID
 xdotool windowactivate $WID
 xdotool windowfocus --sync $WID
 xdotool key --window $WID --clearmodifiers --delay 250 "F9" "F5" "y" "Return" "2"
 sleep 4
-xdotool type --window $WID --clearmodifiers --delay 300 "n:demo"
+xdotool type --window $WID --clearmodifiers --delay 350 "n:demo"
 xdotool key --window	 $WID --clearmodifiers --delay 250 "Return"
-
